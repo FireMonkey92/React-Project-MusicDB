90f60d6bebc05ced692c9e72838c4be827fb7eb8
Tanesh <35836662+FireMonkey92@users.noreply.github.com>
2018-11-15 18:35:56 +0530

    Added Artist List CSS Style Added code in artistList.js to populate the list of artist

--- src/components/artistList.js
+++ src/components/artistList.js
@@ -1,22 +1,29 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
- // We need state less components so...
-const ArtistList= (props) =>{
+import { Link } from 'react-router-dom';
+// We need state less components so...
+const ArtistList = (props) => {
 
-    const list =({allArtists})=>{
-            console.log(allArtists);
+    const list = ({ allArtists }) => {
+        console.log(allArtists);
 
-            if(allArtists){
-                return allArtists.map((item)=>{
-                    return (
-                       <div>item</div>
-                    )  
-               })
-            }
-            
+        // to check that All artists has data or not , if yes , rander divs
+        // Without this it wll throws error as  first time when applictaion get loads , this allArtist variable is empty array
+        if (allArtists) {
+            return allArtists.map((item) => {
+                const style = {
+                    background: `url('images/covers/${item.cover}.jpg') no-repeat`
+                }
+                return (
+                    <Link key={item.id} to={`/artist/${item.id}`} className='artist_item'  style={style}>
+                            <div>{item.name}</div>
+                    </Link>
+
+                )
+            })
         }
+    }
     //console.log(props);
-    return(
+    return (
         <div className="artists_list">
             <h4>Browse The Artists</h4>
             {list(props)}
