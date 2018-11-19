fb4e5da9864c88443e415e857e0f024f952fc182
Tanesh <35836662+FireMonkey92@users.noreply.github.com>
2018-11-19 18:40:16 +0530

    display artist's details on artists.js Added CSS for  header

--- src/components/artists.js
+++ src/components/artists.js
@@ -3,14 +3,52 @@ import React, { Component } from 'react';
 // Componebnts 
 import Header from './header';
 
+const REQ_URL = "http://localhost:3004/artists";
 
 class Artist extends Component {
 
+
+    constructor(props) {
+        super(props);
+        this.state ={
+            artist : ''
+        }
+
+    }
+
+    componentDidMount() {
+        //to get the id from URl, artist id is id we passes in Router:
+        //console.log(this.props.match.params.artistid);
+        fetch(`${REQ_URL}/${this.props.match.params.artistid}`, {
+            method: "GET"
+        })
+            .then(responce => responce.json())
+            .then(json => {
+                
+                this.setState({
+                    artist : json
+                });
+
+                
+            })
+    }
+
     render() {
         return (
-        <div className='detail_container'>
-            <Header></Header>
-        </div>
+            <div className='detail_container'>
+                <Header></Header>
+                <div className='artist_bio'>
+                    <div className='avatar'>
+                    <span style={{background: `url(/images/covers/${this.state.artist.covor}.jpg') no repeat`}}></span>
+                    </div>
+                    <div className='bio'>
+                        <h3>{this.state.artist.name}</h3>
+                        <div className='bio_text'>
+                            {this.state.artist.bio}
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
